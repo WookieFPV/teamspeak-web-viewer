38105448b5044301bcd500ceb29ab0af0284c04b
Lukas Müller <WookieFPV@users.noreply.github.com>
2024-02-04 23:14:58 +0100

    make channel collapsible

--- src/components/TsChannel.tsx
+++ src/components/TsChannel.tsx
@@ -4,6 +4,7 @@ import type {
 } from "ts3-nodejs-library/lib/types/ResponseTypes";
 import { ChevronDownIcon, ChevronRightIcon } from "~/components/tsIcons";
 import { TsUser } from "~/components/TsUser";
+import { useState } from "react";
 
 export const TsChannel = ({
   channel,
@@ -14,40 +15,42 @@ export const TsChannel = ({
   channels: ChannelEntry[];
   clients: ClientEntry[];
 }) => {
-  const clientName: string = channel.channelName;
-  const isSpacer =
-    clientName.includes("[cspacer") || clientName.includes("[*spacer");
-  const hasChildren = channels.some((ch) => ch.pid === channel.cid);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
+  const clientsInChannel = clients.filter((cl) => cl.cid === channel.cid);
+  const subChannels = channels.filter((c) => c.pid === channel.cid);
+  const hasChildren = subChannels.length !== 0;
   return (
     <>
-      <div className="mt-0 flex">
+      <button
+        className="mt-0 flex"
+        onClick={() => setIsCollapsed((old) => !old)}
+      >
         <div className="flex items-center">
-          {isSpacer ? null : hasChildren ? (
+          {isSpacer(channel.channelName) ? null : hasChildren ? (
             <ChevronDownIcon className="mr-2 text-red-600" />
           ) : (
-            <ChevronRightIcon className="ml-3 mr-2 text-green-500" />
+            <ChevronRightIcon className="mr-2 text-green-500" />
           )}
-          <span className="font-sans ">
+          <span className="font-sans">
             {cleanUpSpacerName(channel.channelName)}
           </span>
         </div>
-      </div>
-      {clients
-        .filter((cl) => cl.cid === channel.cid)
-        .map((cl) => (
-          <TsUser key={cl.clid} client={cl} />
-        ))}
-      {channels
-        .filter((c) => c.pid === channel.cid)
-        .map((subCh) => (
-          <TsChannel
-            key={subCh.cid}
-            channel={subCh}
-            channels={channels}
-            clients={clients}
-          />
-        ))}
+      </button>
+      {!isCollapsed &&
+        clientsInChannel.map((cl) => <TsUser key={cl.clid} client={cl} />)}
+      {!isCollapsed && (
+        <div className="ml-2">
+          {subChannels.map((subCh) => (
+            <TsChannel
+              key={subCh.cid}
+              channel={subCh}
+              channels={channels}
+              clients={clients}
+            />
+          ))}
+        </div>
+      )}
     </>
   );
 };
@@ -57,3 +60,6 @@ const spacerRegex = new RegExp(/\[.*spacer.*]/);
 const cleanUpSpacerName = (name: string) => {
   return name.replace(spacerRegex, "");
 };
+
+const isSpacer = (name: string) =>
+  name.includes("[cspacer") || name.includes("[*spacer");
